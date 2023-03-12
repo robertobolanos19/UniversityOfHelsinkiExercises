3b22c7e3cd99f1b2da5f2e9bf6f68beb0a45d94a
Roberto Bolanos <roberto_bolanos@icloud.com>
2023-03-12 15:44:03 -0500

    fixed code for part2 dataforcountries and add codetour to it aswell

--- part2/dataforcountries/src/index.js
+++ part2/dataforcountries/src/index.js
@@ -1,6 +1,9 @@
+//Importing react to the allow all react functionalities to work
 import React from 'react'
+//ReactDom is basically the glue for React and the DOM
 import ReactDOM from 'react-dom/client'
-
+//We are importing App so it can be rendered 
 import App from './App'
 
+//ReactDom.createRoot is used to render its contents into div-element, defined in the file public/html, having the id value 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
