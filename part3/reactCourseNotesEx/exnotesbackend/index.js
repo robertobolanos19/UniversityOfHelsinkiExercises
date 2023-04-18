8a761e0e126a9ef3ec1f58baca203cf22f00e5ba
Roberto Bolanos <roberto_bolanos@icloud.com>
2023-04-18 10:17:14 -0500

    added validation system for post and edit

--- part3/reactCourseNotesEx/exnotesbackend/index.js
+++ part3/reactCourseNotesEx/exnotesbackend/index.js
@@ -19,6 +19,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
+  else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
@@ -52,7 +55,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -64,9 +67,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -80,18 +85,17 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const {content,important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },   
+    { new: true, runValidators: true, context: 'query' }  
+  )  
+  .then(updatedNote => {
       response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  })
+  .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
