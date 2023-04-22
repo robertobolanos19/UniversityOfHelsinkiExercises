3d99d0b89c5e558014076075c49e019549f47ad2
Roberto Bolanos <roberto_bolanos@icloud.com>
2023-04-21 21:40:43 -0500

    completed 4.2

---
+++ part4/blog/backend/app.js
@@ -0,0 +1,37 @@
+//!FINE
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const express = require('express')
+const app =  express()
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
---
+++ part4/blog/backend/utils/config.js
@@ -0,0 +1,11 @@
+//!FINE
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = 
+{
+  MONGODB_URI,
+  PORT
+}
