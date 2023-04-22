6fad2eeedcccb101145760053bac76a86c9018f3
Roberto Bolanos <roberto_bolanos@icloud.com>
2023-04-22 13:14:30 -0500

    4.4 complete

--- part4/blog/backend/utils/list_helper.js
+++ part4/blog/backend/utils/list_helper.js
@@ -1,9 +1,27 @@
+const logger = require('./logger')
 const dummy = (blogs) =>
 {
     return 1
 }
 
+const totalLikes = (blog) =>
+{
+    const blogs = blog.map(blog => blog.likes)
+    let result = blogs.reduce((a,b) => {
+        return a+b
+    })
+
+    logger.info(result)
+    return result
+}
+
+const favoriteBlog = (blog) => 
+{
+    
+}
+
 module.exports = 
 {
-    dummy
+    dummy,
+    totalLikes
 }
