81fc106ec3567c453de1931d760c0dfd0fd2f660
Roberto Bolanos <roberto_bolanos@icloud.com>
2023-06-28 11:37:02 -0500

    renamed main folder part4 to part5

--- part5/reactCourseNotesEx/exnotesfrontend/src/components/Footer.js
+++ part5/reactCourseNotesEx/exnotesfrontend/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
       <div style={footerStyle}>
         <br />
-        <em>Note app, Roberto Bolanos 2023!</em>
+        <em>Note app, Roberto Bolanos 2023</em>
       </div>
     )
   }
